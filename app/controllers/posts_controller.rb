75cc226690e83433290af9a36a481ef29e43598d
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2018-03-14 10:28:51 +0900

    imprement basic actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:index, :show, :edit, :update]
 
   def index
   end
@@ -19,6 +20,11 @@ class PostsController < ApplicationController
   end
 
   def destory
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
