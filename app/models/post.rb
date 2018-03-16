05e0defe4f4ef4684709d61b27eaaace1dd45a4b
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2018-03-16 20:43:41 +0900

    set for using accepts_nested_attributes_for

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many   :text_contents
   has_many   :textarea_contents
   has_many   :date_contents
+
+  accepts_nested_attributes_for :text_contents
+  accepts_nested_attributes_for :textarea_contents
+  accepts_nested_attributes_for :date_contents
 end
