a6d4321347c169bdf2466402bb86b06fc1f39fe1
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2018-03-16 20:00:30 +0900

    check text_contents succeeds to save

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,24 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:index, :show, :edit, :update]
+  before_action :set_tags, only: [:new, :create]
 
   def index
     @tag = TagManagement.where(tag: @post.tag).order(:order)
   end
 
   def new
-    @tag_name = '映画'
-    @tag = TagManagement.where(tag: @tag_name).order(:order)
   end
 
   def create
     post = Post.new(post_params)
     if post.save
-      redirect_to posts_path
+      text_contents = TextContent.new(content: text_contents_params["タイトル"], post_id: post.id)
+      if text_contents.save
+        redirect_to posts_path
+      else
+        p text_contents.errors.full_messages
+        redirect_to new_post_path
+      end
     else
       render 'new'
     end
@@ -36,8 +41,17 @@ class PostsController < ApplicationController
     @post = Post.find(1)
   end
 
+  def set_tags
+    @tag_name = '映画'
+    @tags = TagManagement.where(tag: @tag_name).order(:order)
+  end
+
   def post_params
     params.permit(:tag, :user_id)
+  end
+
+  def text_contents_params
+    params.permit("タイトル".to_sym)
   end
 
 end
