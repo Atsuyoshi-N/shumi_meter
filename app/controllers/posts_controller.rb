75d7d59a29016c0088e0ba0e95c39873bd7df54b
wkwkes <wkwkes@gmail.com>
2018-03-20 17:04:12 +0900

    update to show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:index, :show, :edit, :update]
-  before_action :set_tags, only: [:new, :create]
+  before_action :set_tags, only: [:new, :create, :index]
 
   def index
-    @tag = TagManagement.where(tag: @post.tag).order(:order)
+    @posts = Post.all
+    @data = formalize_posts(Post.all)
   end
 
   def new
@@ -33,7 +34,7 @@ class PostsController < ApplicationController
 
   private
   def set_post
-    @post = Post.find(1)
+    @post = Post.find(1)  
   end
 
   def set_tags
@@ -43,6 +44,34 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:tag, :user_id, text_contents_attributes: [:content, :label], textarea_contents_attributes: [:content, :label], date_contents_attributes: [:content, :label])
+  end
+
+  def formalize_post(p)
+    tags = TagManagement.where(tag: @tag_name).order(:order)
+    data = []
+    tags.each do |t|
+      if t.datatype == "text_contents" then 
+        cont = TextContent.find_by(post_id: p.id, label: t.label)
+        data.push({"datatype" => "text_contents", "label" => cont.label, "content" => cont.content})
+      elsif t.datatype == "textarea_contents" then
+        cont = TextareaContent.find_by(post_id: p.id, label: t.label)
+        data.push({"datatype" => "text_contents", "label" => cont.label, "content" => cont.content})
+      elsif t.datatype == "date_contents" then
+        cont = DateContent.find_by(post_id: p.id, label: t.label)
+        data.push({"datatype" => "text_contents", "label" => cont.label, "content" => cont.content})
+      else
+        fail
+      end
+    end
+    return {"tag" => p.tag, "data" => data}
+  end
+
+  def formalize_posts(n)
+    data = []
+    n.each do |p|
+      data.push(formalize_post (p))
+    end
+    return data
   end
 
 end
