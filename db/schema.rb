8acbd86d1664d0ab238d67300e62dfe4222586ef
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2018-03-20 16:41:47 +0900

    create image_contents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314032451) do
+ActiveRecord::Schema.define(version: 20180320071020) do
 
   create_table "date_contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "post_id"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20180314032451) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_date_contents_on_post_id"
+  end
+
+  create_table "image_contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
